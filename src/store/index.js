fd06d2b678141624c5a5529d76d32b84141809e8
Kiwi <chenyujie217@163.com>
2025-10-29 23:39:43 -0400

    debug

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
-import todosReducer from './todosSlice';
-import { composeWithDevTools } from '@redux-devtools/extension';
-
-export const store = configureStore({
-  reducer: {
-    todos: todosReducer,
-  },
-  enhancers: (getDefaultEnhancers) =>
-    getDefaultEnhancers().concat(composeWithDevTools()),
-});
+import {configureStore} from '@reduxjs/toolkit'; 
+import todosReducer from './todosSlice'; 
+ 
+export const store = configureStore({ 
+  reducer: { 
+    todos: todosReducer, 
+  }, 
+}); 
